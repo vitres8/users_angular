7f863f4acddde63a4abc2f95fa937f8d8d75ad59
vitres8 <vitres8@gmail.com>
2024-10-10 19:01:10 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserAppComponent } from './components/user-app.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserAppComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = '5-user-app';
+  title = 'user-app';
 }
---
+++ src/app/components/user-app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-app.component.html',
+
+})
+export class UserAppComponent {
+
+  title: string = 'Listado de usuarios!'
+
+}
